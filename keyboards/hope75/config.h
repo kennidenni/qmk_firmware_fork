099067702e4ca748e60bea79111818663a918142
Kenneth Apeland <kenape96@outlook.com>
2024-04-06 22:43:51 +0200

    added hope75

---
+++ keyboards/hope75/config.h
@@ -0,0 +1,13 @@
+#pragma once
+
+/* Mechanical locking support. Use KC_LCAP, KC_LNUM or KC_LSCR instead in keymap */
+#define LOCKING_SUPPORT_ENABLE
+
+/* Locking resynchronize hack */
+#define LOCKING_RESYNC_ENABLE
+
+#define RGBLIGHT_LAYERS
+
+#define ENABLE_COMPILE_KEYCODE
+
+#define RGBLIGHT_DEFAULT_HUE 169
